61bf011ff53ff8af3e35f72f581c506ce9c53f11
Kohei Tamura <ktamura.biz.80@gmail.com>
2017-06-26 13:39:42 +0900

    Add AssertionErrorServlet

---
+++ src/main/java/org/t246osslab/easybuggy4sb/errors/AssertionErrorController.java
@@ -0,0 +1,13 @@
+package org.t246osslab.easybuggy4sb.errors;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AssertionErrorController {
+
+    @RequestMapping(value = "/asserr")
+    public void process() {
+        assert 1 >= 2 : "Invalid!";
+    }
+}
