09aeaf040cec058da9fc40b1af68f8dede60f1e3
Kohei Tamura <ktamura.biz.80@gmail.com>
2017-06-27 18:39:31 +0900

    Add a controller that can cause code injection

---
+++ src/main/java/org/t246osslab/easybuggy4sb/vulnerabilities/CodeInjectionController.java
@@ -0,0 +1,60 @@
+package org.t246osslab.easybuggy4sb.vulnerabilities;
+
+import java.util.Locale;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+import org.apache.commons.lang.StringUtils;
+import org.owasp.esapi.ESAPI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class CodeInjectionController {
+
+	private static final Logger log = LoggerFactory.getLogger(SQLInjectionController.class);
+
+	@Autowired
+	MessageSource msg;
+
+	@RequestMapping(value = "/codeijc")
+	public ModelAndView process(@RequestParam(value = "jsonString", required = false) String jsonString,
+			ModelAndView mav, Locale locale) {
+		mav.setViewName("codeinjection");
+		mav.addObject("title", msg.getMessage("title.parse.json", null, locale));
+		try {
+			if (!StringUtils.isBlank(jsonString)) {
+				jsonString = jsonString.replaceAll(" ", "");
+				jsonString = jsonString.replaceAll("\r\n", "");
+				jsonString = jsonString.replaceAll("\n", "");
+				try {
+					ScriptEngineManager manager = new ScriptEngineManager();
+					ScriptEngine scriptEngine = manager.getEngineByName("JavaScript");
+					scriptEngine.eval("JSON.parse('" + jsonString + "')");
+					mav.addObject("msg", msg.getMessage("msg.valid.json", null, locale));
+				} catch (ScriptException e) {
+					mav.addObject("errmsg", msg.getMessage("msg.invalid.json",
+							new String[] { e.getMessage() }, null, locale));
+				} catch (Exception e) {
+					log.error("Exception occurs: ", e);
+					mav.addObject("errmsg", msg.getMessage("msg.invalid.json",
+							new String[] { e.getMessage() }, null, locale));
+				}
+			} else {
+				mav.addObject("msg", msg.getMessage("msg.enter.json.string", null, locale));
+			}
+
+		} catch (Exception e) {
+			log.error("Exception occurs: ", e);
+		}
+		return mav;
+	}
+}
