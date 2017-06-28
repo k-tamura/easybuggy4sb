c8e9f9a219e9fb200ed04dce02806b9d731c4d57
Kohei Tamura <ktamura.biz.80@gmail.com>
2017-06-28 17:28:27 +0900

    Add a controller that allows brute force attacks

---
+++ src/main/java/org/t246osslab/easybuggy4sb/vulnerabilities/BruteForceController.java
@@ -0,0 +1,55 @@
+package org.t246osslab.easybuggy4sb.vulnerabilities;
+
+import java.io.IOException;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.t246osslab.easybuggy4sb.controller.DefaultLoginController;
+
+@Controller
+public class BruteForceController extends DefaultLoginController {
+
+    @Autowired
+    MessageSource msg;
+
+    @RequestMapping(value = "/bruteforce/login", method = RequestMethod.GET)
+    public ModelAndView doGet(ModelAndView mav, HttpServletRequest req, HttpServletResponse res, Locale locale) {
+        req.setAttribute("note", msg.getMessage("msg.note.brute.force", null, locale));
+        super.doGet(mav, req, res, locale);
+        return mav;
+    }
+
+    @RequestMapping(value = "/bruteforce/login", method = RequestMethod.POST)
+    public ModelAndView doPost(ModelAndView mav, HttpServletRequest req, HttpServletResponse res, Locale locale) throws IOException {
+
+        String userid = req.getParameter("userid");
+        String password = req.getParameter("password");
+
+        HttpSession session = req.getSession(true);
+        if (authUser(userid, password)) {
+            session.setAttribute("authNMsg", "authenticated");
+            session.setAttribute("userid", userid);
+
+            String target = (String) session.getAttribute("target");
+            if (target == null) {
+                res.sendRedirect("/admins/main");
+            } else {
+                session.removeAttribute("target");
+                res.sendRedirect(target);
+            }
+        } else {
+            session.setAttribute("authNMsg", "msg.authentication.fail");
+            doGet(mav, req, res, locale);
+        }
+        return mav;
+    }
+}
