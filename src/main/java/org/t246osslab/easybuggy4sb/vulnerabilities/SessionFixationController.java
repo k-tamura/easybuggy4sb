c639aed9a84c90af97e4e29f086f6d1f3d93c1a7
Kohei Tamura <ktamura.biz.80@gmail.com>
2017-06-28 18:54:36 +0900

    Add a controller that can cause session fixation

---
+++ src/main/java/org/t246osslab/easybuggy4sb/vulnerabilities/SessionFixationController.java
@@ -0,0 +1,39 @@
+package org.t246osslab.easybuggy4sb.vulnerabilities;
+
+import java.io.IOException;
+import java.util.Locale;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.t246osslab.easybuggy4sb.controller.DefaultLoginController;
+
+@Controller
+public class SessionFixationController extends DefaultLoginController {
+
+    private static final Logger log = LoggerFactory.getLogger(SessionFixationController.class);
+
+    @Autowired
+    MessageSource msg;
+
+    @RequestMapping(value = "/sessionfixation/login", method = RequestMethod.GET)
+    public ModelAndView doGet(ModelAndView mav, HttpServletRequest req, HttpServletResponse res, Locale locale) {
+        req.setAttribute("note", msg.getMessage("msg.note.session.fixation", null, locale));
+        super.doGet(mav, req, res, locale);
+        return mav;
+    }
+
+    @RequestMapping(value = "/sessionfixation/login", method = RequestMethod.POST)
+    public ModelAndView doPost(ModelAndView mav, HttpServletRequest req, HttpServletResponse res, Locale locale) throws IOException, ServletException {
+        return super.doPost(mav, req, res, locale);
+    }
+}
