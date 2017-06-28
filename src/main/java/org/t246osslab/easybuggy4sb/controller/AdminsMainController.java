c0f80d2eb2ad82b5262dccef4984360c6209f062
Kohei Tamura <ktamura.biz.80@gmail.com>
2017-06-28 14:42:57 +0900

    Add a controller that shows admin main page

---
+++ src/main/java/org/t246osslab/easybuggy4sb/controller/AdminsMainController.java
@@ -0,0 +1,29 @@
+package org.t246osslab.easybuggy4sb.controller;
+
+import java.util.Locale;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.t246osslab.easybuggy4sb.core.model.User;
+
+@Controller
+public class AdminsMainController {
+
+	/* User's login history using in-memory account locking */
+	protected ConcurrentHashMap<String, User> userLoginHistory = new ConcurrentHashMap<String, User>();
+	
+	@Autowired
+	MessageSource msg;
+
+	@RequestMapping(value = "/admins/main")
+	public ModelAndView doGet(ModelAndView mav, Locale locale) {
+
+		mav.setViewName("adminmain");
+		mav.addObject("title", msg.getMessage("title.admins.main.page", null, locale));
+		return mav;
+	}
+}
