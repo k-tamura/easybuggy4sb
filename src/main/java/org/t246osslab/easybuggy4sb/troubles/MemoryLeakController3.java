04047d68b30bba521fcaecbb8ee5233a6ec3c6f2
Kohei Tamura <ktamura.biz.80@gmail.com>
2017-06-29 13:49:12 +0900

    Add a servlet that can cause C heap memory leaks

---
+++ src/main/java/org/t246osslab/easybuggy4sb/troubles/MemoryLeakController3.java
@@ -0,0 +1,40 @@
+package org.t246osslab.easybuggy4sb.troubles;
+
+import java.util.Locale;
+import java.util.TimeZone;
+import java.util.zip.Deflater;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javassist.CannotCompileException;
+
+@Controller
+public class MemoryLeakController3 {
+
+	@Autowired
+	MessageSource msg;
+
+	@RequestMapping(value = "/memoryleak3")
+	public ModelAndView process(ModelAndView mav, Locale locale) throws CannotCompileException {
+		mav.setViewName("memoryleak3");
+		mav.addObject("title", msg.getMessage("title.timezone", null, locale));
+        mav.addObject("timeZone", TimeZone.getDefault());
+        toDoRemove();
+		return mav;
+	}
+
+	private void toDoRemove() throws CannotCompileException {
+        String inputString = "inputString";
+        byte[] input = inputString.getBytes();
+        byte[] output = new byte[100];
+        for (int i = 0; i < 1000; i++) {
+            Deflater compresser = new Deflater();
+            compresser.setInput(input);
+            compresser.deflate(output);
+        }
+	}
+}
