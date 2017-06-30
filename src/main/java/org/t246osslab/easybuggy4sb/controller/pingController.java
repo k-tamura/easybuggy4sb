70b132d0be69393f1d24f7fb0181a755182ee803
Kohei Tamura <ktamura.biz.80@gmail.com>
2017-06-30 13:37:50 +0900

    Remove HelloController and create PingController

---
+++ src/main/java/org/t246osslab/easybuggy4sb/controller/pingController.java
@@ -0,0 +1,15 @@
+package org.t246osslab.easybuggy4sb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class pingController {
+
+	@RequestMapping(value = "/ping")
+	public ModelAndView hello(ModelAndView mav) {
+		mav.setViewName("ping");
+		return mav;
+	}
+}
