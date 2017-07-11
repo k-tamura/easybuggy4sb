5c1dc15dec5630a8a5f2b45a07e01b4a0d51ed08
Kohei <ktamura.biz.80@gmail.com>
2017-07-11 21:24:51 +0900

    Fix the filter to cause mojibake

--- src/main/java/org/t246osslab/easybuggy4sb/core/filters/EncodingFilter.java
+++ src/main/java/org/t246osslab/easybuggy4sb/core/filters/EncodingFilter.java
@@ -2,60 +2,34 @@ package org.t246osslab.easybuggy4sb.core.filters;
 
 import java.io.IOException;
 
-import javax.servlet.Filter;
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.boot.web.filter.OrderedCharacterEncodingFilter;
 import org.springframework.stereotype.Component;
 
 /**
  * Servlet Filter for encoding
  */
 @Component
-public class EncodingFilter implements Filter {
+public class EncodingFilter extends OrderedCharacterEncodingFilter {
 
-	/**
-	 * Default constructor.
-	 */
-	public EncodingFilter() {
-		// Do nothing
-	}
-
-	/**
-	 * Call
-	 * {@link #doFilter(HttpServletRequest, HttpServletResponse, FilterChain)}.
-	 * 
-	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-	 */
-	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
-			throws IOException, ServletException {
-		HttpServletRequest request = (HttpServletRequest) req;
-		if (!"/mojibake".equals(request.getRequestURI())) {
-			/*
-			 * Set the default character encoding and content type to UTF-8
-			 * (except under /mojibake)
-			 */
-			req.setCharacterEncoding("UTF-8");
-			res.setContentType("text/html; charset=UTF-8");
-		} else {
-			req.setCharacterEncoding("euc-kr");
-			res.setContentType("text/html; charset=euc-kr");
-		}
-		chain.doFilter(req, res);
-	}
-
-	@Override
-	public void destroy() {
-		// Do nothing
-	}
-
-	@Override
-	public void init(FilterConfig arg0) throws ServletException {
-		// Do nothing
-	}
+    /**
+     * @see org.springframework.web.filter.CharacterEncodingFilter#doFilterInternal(javax.servlet.http.HttpServletRequest,
+     *      javax.servlet.http.HttpServletResponse, javax.servlet.FilterChain)
+     */
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        super.setOrder(HIGHEST_PRECEDENCE);
+        if ("/mojibake".equals(request.getRequestURI())) {
+            super.setEncoding("Shift_JIS");
+        } else {
+            super.setEncoding("UTF-8");
+        }
+        super.setForceEncoding(true);
+        super.doFilterInternal(request, response, filterChain);
+    }
 }
