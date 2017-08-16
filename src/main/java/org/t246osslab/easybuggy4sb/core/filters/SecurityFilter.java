cd8b186794a86abe6fe13bb17d350e92f1e58ca0
Kohei Tamura <ktamura.biz.80@gmail.com>
2017-08-16 12:52:32 +0900

    Fix a bug of Security filter

--- src/main/java/org/t246osslab/easybuggy4sb/core/filters/SecurityFilter.java
+++ src/main/java/org/t246osslab/easybuggy4sb/core/filters/SecurityFilter.java
@@ -42,8 +42,9 @@ public class SecurityFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
         String target = request.getRequestURI();
 
-        /* Prevent to upload large files if target does not start w/ /ursupload ... */
-        if (!target.startsWith("/ursupload") && request.getMethod().equalsIgnoreCase("POST")) {
+        /* Prevent to upload large files if target does not start w/ /ureupload ... */
+        if ((target.startsWith("/ureupload") || target.startsWith("/xee") || target.startsWith("/xxe"))
+                && request.getMethod().equalsIgnoreCase("POST")) {
             ServletFileUpload upload = new ServletFileUpload();
             upload.setFileItemFactory(new DiskFileItemFactory());
             upload.setFileSizeMax(1024 * 1024 * 10); // 10MB
