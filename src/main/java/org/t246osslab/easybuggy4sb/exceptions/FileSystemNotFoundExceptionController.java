0a52b6a565f7037e79c60f42434f2d57c014ba85
k-tamura <ktamura.biz.80@gmail.com>
2018-03-19 22:52:20 +0900

    Add a Controller that throws FileSystemNotFoundException

---
+++ src/main/java/org/t246osslab/easybuggy4sb/exceptions/FileSystemNotFoundExceptionController.java
@@ -0,0 +1,26 @@
+package org.t246osslab.easybuggy4sb.exceptions;
+
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.FileSystems;
+import java.util.HashMap;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.t246osslab.easybuggy4sb.controller.AbstractController;
+
+@Controller
+public class FileSystemNotFoundExceptionController  extends AbstractController {
+
+	@RequestMapping(value = "/fsnfe")
+	public void process() {
+		URI uri = URI.create("jar:file:/not/exist.zip");
+		if (!"file".equals(uri.getScheme())) {
+			try {
+				FileSystems.newFileSystem(uri, new HashMap<String, Object>());
+			} catch (IOException e) {
+				log.error("IOException occurs: ", e);
+			}
+		}
+	}
+}
