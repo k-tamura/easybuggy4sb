7ac5928f97e48d6f57cc0c0bcb2e7701bc2f00fd
k-tamura <ktamura.biz.80@gmail.com>
2025-11-25 16:57:09 +0900

    Remove unnecessary imports

--- src/main/java/org/t246osslab/easybuggy4sb/vulnerabilities/UnrestrictedExtensionUploadController.java
+++ src/main/java/org/t246osslab/easybuggy4sb/vulnerabilities/UnrestrictedExtensionUploadController.java
@@ -4,10 +4,6 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Locale;
 
 import javax.imageio.ImageIO;
